23f1883590c6cc76987d0980b75144391b1122c4
OrangeTacTics <>
2021-09-12 18:36:24 -0700

    Add nodelay to quiz settings.

--- chairmanmao/chairmanmao.py
+++ chairmanmao/chairmanmao.py
@@ -235,7 +235,7 @@ async def cmd_quiz(ctx):
         msg = 'Currently, only HSK 1 and 2 quizzes are available.'
     else:
         num_questions = SCORE_LIMIT_BY_DECK.get(f'dmt_hsk{aiming_for}')
-        msg = f'For the next quiz, use:\n`k!quiz dmt_hsk{aiming_for} mmq=1 atl=10 {num_questions}`'
+        msg = f'For the next quiz, use:\n`k!quiz dmt_hsk{aiming_for} nodelay mmq=1 atl=10 {num_questions}`'
 
     await ctx.send(msg)
 
