7c1fbf39542297b8d59b11b16a416233e85d529a
OrangeTacTics <>
2021-10-14 22:21:39 -0700

    Change !welcomeme to use ! instead of $.

--- chairmanmao/cogs/welcome.py
+++ chairmanmao/cogs/welcome.py
@@ -41,7 +41,7 @@ class WelcomeCog(ChairmanMaoCog):
             "The numbers in our members' usernames are their social credit score. Be a good citizen, "
             "and you will see your social credit increase.",
             '',
-            'You may use the `$learner` command to give yourself the @中文学习者 (Chinese learner) role. '
+            'You may use the `!learner` command to give yourself the @中文学习者 (Chinese learner) role. '
             'This will give you access to our learning resources and to take exams.',
             '',
             'For more information about the various channels: https://dailymandarinthread.info/discord/channels/',
@@ -50,19 +50,19 @@ class WelcomeCog(ChairmanMaoCog):
             '',
             'Some good commands to know about:',
             '```',
-            '    $name',
+            '    !name',
             '        Change your nickname.',
             '',
-            '    $draw 猫',
+            '    !draw 猫',
             '        Draws a character.',
             '',
-            '    $learner',
+            '    !learner',
             '        Grants yourself the @中文学习者 role.',
             '',
-            '    $exam',
+            '    !exam',
             '        Test your Chinese abilities. @中文学习者 only.',
             '',
-            '    $leaderboard',
+            '    !leaderboard',
             '        Shows the social credit leaderboard.',
             '```',
         ]
