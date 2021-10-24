9cad3c26a360029b00437bff3f2edd4adcd556ec
OrangeTacTics <>
2021-10-23 21:26:41 -0700

    Create server-testing script to run a test server against the TEST database on port 9666.

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,4 @@
-from .server import app
+from .server import make_app
 
 
 def main():
@@ -9,7 +9,24 @@ def main():
     load_dotenv()
 
     uvicorn.run(
-        app,
+        make_app(),
         host=os.environ.get("SERVER_HOST", "127.0.0.1"),
         port=int(os.environ.get("SERVER_PORT", "8002")),
+    )
+
+
+def main_test():
+    import os
+    import uvicorn
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    os.environ['MONGODB_URL'] = os.environ['MONGODB_TEST_URL']
+    os.environ['MONGODB_DB'] = os.environ['MONGODB_TEST_DB']
+
+    uvicorn.run(
+        make_app(),
+        host=os.environ.get("SERVER_HOST", "127.0.0.1"),
+        port=int(os.environ.get("SERVER_PORT", "9666")),
     )
