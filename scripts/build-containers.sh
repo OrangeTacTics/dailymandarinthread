f5663b35b3715382d6fe4b92725c75cbfd35a557
OrangeTacTics <>
2021-11-13 18:07:37 -0700

    Fix up CirleCI packaging.

--- scripts/build-containers.sh
+++ scripts/build-containers.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+set -xe
+
+REGISTRY=${1:-"localhost"}
+
 podman build \
-    -t dmt-chairmanmao:latest \
+    -t $REGISTRY/dmt-chairmanmao:latest \
     -f dmt_chairmanmao/Dockerfile
 
 podman build \
-    -t dmt-graphql:latest \
+    -t $REGISTRY/dmt-graphql:latest \
     -f dmt_graphql/Dockerfile
 
 podman build \
-    -t dmt-auth:latest \
+    -t $REGISTRY/dmt-auth:latest \
     -f dmt_auth/Dockerfile
 
 podman build \
-    -t dmt-dailymandarinthreadinfo:latest \
+    -t $REGISTRY/dmt-dailymandarinthreadinfo:latest \
     -f dmt_dailymandarinthreadinfo/Dockerfile
